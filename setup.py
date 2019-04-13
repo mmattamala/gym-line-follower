4cf6d9daaf28f45979d3e5182222d442a15d3c12
Matías Mattamala <mmattamala@ug.uchile.cl>
2019-04-12 23:33:14 -0400

    Minor fixes in installation instructions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_line_follower',
       version='0.1.0',
-      install_requires=['gym', 'pybullet', 'opencv-python', 'shapely', 'numpy'],
+      install_requires=['gym', 'pybullet', 'opencv-python', 'shapely', 'numpy', 'keras', 'keras-rl'],
       author="Nejc Planinsek",
       author_email="planinseknejc@gmail.com",
       description="Line follower simulator environment.",
